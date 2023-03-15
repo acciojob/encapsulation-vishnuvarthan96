994669c5d11090c9aa75dd1c10253bac6d9125ea
vishnuvarthan <vishnuv9677@gmail.com>
2023-03-15 22:13:45 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+     public static void main(String[] args){
+          RWOnly RW =new RWOnly();
+          RW.setter("Acciojob");
+          String name=RW.getter();
+          System.out.println(name);
+     }
 }
