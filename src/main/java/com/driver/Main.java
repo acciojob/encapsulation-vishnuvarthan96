aaa0ff9df74e37caab562ff21d3d01d9996cb43e
vishnu <vishnuv9677@gmail.com>
2023-04-15 12:19:35 +0530

    Assignment rework

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
      public static void main(String[] args){
           RWOnly RW =new RWOnly();
-          RW.setter("Acciojob");
-          String name=RW.getter();
+          RW.setName("Acciojob");
+          String name=RW.getName();
           System.out.println(name);
      }
 }
